010064aa18e153bcdd98573b74b0fd28037afdb1
Martin Levington <LevingtM@ribac.net>
2019-01-15 09:52:40 +0000

    update to .netcore 2.1

---
+++ ScrumPm/ScrumPm.Application.Tests.Unit/Teams/TeamApplicationServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScrumPm.Application.Tests.Unit.Teams
+{
+    public class TeamApplicationServiceTests
+    {
+
+        public TeamApplicationServiceTests()
+        {
+
+        }
+
+    }
+}
